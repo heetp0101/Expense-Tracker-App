12a8c9b5277e6e0d6719657eb31c85fb84f369e5
Patel Heet <heetp0101@gmail.com>
2026-01-17 17:37:22 +0530

    changed App.js file

--- src/App.js
+++ src/App.js
@@ -10,25 +10,12 @@ import Index from './pages/Index';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Test from './pages/Test';
-
+import {auth} from './firebase'
 
 function App() {
 
 
-  const firebaseConfig = {
-    apiKey: process.env.API_KEY,
-    authDomain: process.env.AUTH_DOMAIN,
-    databaseURL: process.env.DATABASE_URL,
-    projectId: process.env.PROJECT_ID,
-    storageBucket: process.env.STORAGE_BUCKET,
-    messagingSenderId: process.env.MESSAGING_SENDER_ID,
-    appId: process.env.APP_ID,
-    measurementId: process.env.MEASUREMENT_ID
-};
 
-const app = initializeApp(firebaseConfig);
-
-  const auth=getAuth(app);
   let flag = false;
 
   const [IsLoggedIn, setIsLoggedIn] = useState(flag);
